6fd66ea7dc353af85ec69dabe1bf3e8612e0df84
Garry <garry.tamhuang@gmail.com>
2017-08-04 20:02:59 -0400

    checkpoint submission: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -43,6 +43,18 @@ class Kele
     puts response
   end
 
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    response = self.class.post(api_url("checkpoint_submissions"),
+      body: {
+        "checkpoint_id": checkpoint_id,
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "comment": comment
+      },
+      headers: { "authorization" => @auth_token} )
+    puts response
+  end
+
   private
   def api_url(endpoint)
     "https://www.bloc.io/api/v1/#{endpoint}"
